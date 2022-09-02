7bb827f7614e7cb2fa5015469df9f5ac371f0147
kurodayuuki <kurodayuki07122170@gmail.com>
2022-09-02 18:01:49 +0900

    引数の練習

---
+++ Chapter_0006/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Chapter_0005
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numberList = new Int32[5];
+            numberList[0] = 3;
+            numberList[1] = 7;
+            numberList[2] = 8;
+            numberList[3] = 10;
+            numberList[4] = 12;
+
+            //引数。値を引き渡す。
+            ShowTakenokoList(numberList, "きのこ");
+        }
+
+        private static void ShowTakenokoList(Int32[] numberList , string name)
+        {
+            for (int i = 0; i < numberList.Length; i++)
+            {
+                Console.WriteLine(name + "の数は" + numberList[i] + "です。");
+            }
+            Console.ReadLine();
+        }
+    }
+}
