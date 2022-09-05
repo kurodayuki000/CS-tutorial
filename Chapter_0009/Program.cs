92020f0475768445f6c419f736778a5b603246d0
kurodayuuki <kurodayuki07122170@gmail.com>
2022-09-05 23:54:15 +0900

    create csv faile

--- Chapter_0009/Program.cs
+++ Chapter_0009/Program.cs
@@ -1,25 +1,63 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 //パソコンとは → 画面,ハードディスク,メモリ, CPU
 //プログラム → パソコンを操作する
 
 
 
-namespace Chapter_0012
+namespace Chapter_0009
 {
     class Program
     {
         static void Main(string[] args)
         {
-            //ファイルを生成するプログラム
+            var body = System.IO.File.ReadAllText("/Users/kurodayuuki/Desktop/MyText2.csv");
 
-            var body = "名前,年齢,性別,学部,役職";
-            body = body + Environment.NewLine + "新井,19,女,工学部,なし";
-            System.IO.File.WriteAllText("/Users/kurodayuuki/Desktop/MyText.csv", body);
-            Console.WriteLine("保存しました");
+            var sr = new StringReader(body);
+            var nameList = new String[14];
+            var index = 0;
+            while (sr.Peek() != -1)
+            {
+                var line = sr.ReadLine();
+                if (index == 0)
+                {
+                    index++;
+                    continue;
+                }
+                var nameCount = line.IndexOf(',');
+                nameList[index - 1] = line.Substring(0, nameCount);
+                index++;
+                Console.WriteLine(line);
+            }
+            var newBody = "";
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                newBody = newBody + nameList[i] + Environment.NewLine;
+            }
+            EncodingProvider provider = System.Text.CodePagesEncodingProvider.Instance;
+            var encoding = provider.GetEncoding(932);
+            System.IO.File.WriteAllText("/Users/kurodayuuki/Desktop/nameList.csv", newBody, encoding);
 
-            Console.WriteLine("Enterキーで終了します。");
+            Console.WriteLine("Enterで終了します。");
             Console.ReadLine();
+
         }
+
+        //private static void CreateCSVFile()
+        //{
+        //    //ファイルを生成するプログラム
+        //    EncodingProvider provider = System.Text.CodePagesEncodingProvider.Instance;
+        //    var encoding = provider.GetEncoding(932);
+
+        //    var body = "名前,年齢,性別,学部,役職";
+        //    body = body + Environment.NewLine + "新井,19,女,工学部,なし";
+        //    System.IO.File.WriteAllText("/Users/kurodayuuki/Desktop/MyText.csv", body, encoding);
+        //    Console.WriteLine("保存しました");
+
+        //    Console.WriteLine("Enterキーで終了します。");
+        //    Console.ReadLine();
+        //}
     }
 }
