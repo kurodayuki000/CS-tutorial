5222f584607fab1a002b299028e945e17cc2ef5b
kurodayuuki <kurodayuki07122170@gmail.com>
2022-09-05 22:13:28 +0900

    create CSV

---
+++ Chanpter_0009/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+//パソコンとは → 画面,ハードディスク,メモリ, CPU
+//プログラム → パソコンを操作する
+
+
+
+namespace Chapter_0012
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //ファイルを生成するプログラム
+
+            var body = "名前,年齢,性別,学部,役職";
+            body = body + Environment.NewLine + "新井,19,女,工学部,なし";
+            System.IO.File.WriteAllText("/Users/kurodayuuki/Desktop/MyText.csv", body);
+            Console.WriteLine("保存しました");
+
+            Console.WriteLine("Enterキーで終了します。");
+            Console.ReadLine();
+        }
+    }
+}
