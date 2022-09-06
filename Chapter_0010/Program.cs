4fc917503566c3aa3bfdaeb732f436a7e3a3d53e
kurodayuuki <kurodayuki07122170@gmail.com>
2022-09-06 17:27:30 +0900

    文字列操作

---
+++ Chapter_0010/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Chapter_0010
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var body = File.ReadAllText("/Users/kurodayuuki/Desktop/text.txt");
+            var ss = body.Split(" ");
+            var count = 0;
+
+            for (int i = 0; i < ss.Length; i++)
+            {
+                if (ss[i].Contains("a"))
+                {
+                    Console.WriteLine(ss[i]);
+                    count++;
+                }
+            }
+            Console.WriteLine(count + "文字ありました！！！");
+
+            Console.WriteLine("Enterキーで終了");
+            Console.ReadLine();
+        }
+    }
+}
