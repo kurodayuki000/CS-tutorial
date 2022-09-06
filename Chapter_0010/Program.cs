3c66eb4b294fdbd7f9cc08a2d6543ecd11fef1d0
kurodayuuki <kurodayuki07122170@gmail.com>
2022-09-06 17:49:37 +0900

    メソッドのテスト

--- Chapter_0010/Program.cs
+++ Chapter_0010/Program.cs
@@ -7,6 +7,25 @@ namespace Chapter_0010
         static void Main(string[] args)
         {
             var body = File.ReadAllText("/Users/kurodayuuki/Desktop/text.txt");
+            var ss = body.Split(' ');
+
+            for (int i = 0; i < ss.Length; i++)
+            {
+                var index = ss[i].IndexOf('a');
+                if (index == -1) continue;
+
+                Console.WriteLine("--------------------");
+                Console.WriteLine(ss[i]);
+                Console.WriteLine( (index + 1) + "文字目にaがあるよ！");
+            }
+
+            Console.WriteLine("Enterキーで終了");
+            Console.ReadLine();
+        }
+
+        private static void Sample_Cointains()
+        {
+            var body = File.ReadAllText("/Users/kurodayuuki/Desktop/text.txt");
             var ss = body.Split(" ");
             var count = 0;
 
@@ -18,7 +37,47 @@ namespace Chapter_0010
                     count++;
                 }
             }
-            Console.WriteLine(count + "文字ありました！！！");
+            Console.WriteLine(count + "単語ありました！！！");
+
+            Console.WriteLine("Enterキーで終了");
+            Console.ReadLine();
+        }
+
+        private void Sample_StartWith()
+        {
+            var body = File.ReadAllText("/Users/kurodayuuki/Desktop/text.txt");
+            var ss = body.Split(" ");
+            var count = 0;
+
+            for (int i = 0; i < ss.Length; i++)
+            {
+                if (ss[i].StartsWith("y"))
+                {
+                    Console.WriteLine(ss[i]);
+                    count++;
+                }
+            }
+            Console.WriteLine(count + "単語ありました！！！");
+
+            Console.WriteLine("Enterキーで終了");
+            Console.ReadLine();
+        }
+
+        private void Sample_Equals()
+        {
+            var body = File.ReadAllText("/Users/kurodayuuki/Desktop/text.txt");
+            var ss = body.Split(" ");
+            var count = 0;
+
+            for (int i = 0; i < ss.Length; i++)
+            {
+                if (ss[i].Equals("are"))
+                {
+                    Console.WriteLine(ss[i]);
+                    count++;
+                }
+            }
+            Console.WriteLine(count + "単語ありました！！！");
 
             Console.WriteLine("Enterキーで終了");
             Console.ReadLine();
